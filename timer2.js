c251364d23812fd2c066b9dcd183c83c8e68d1f8
Rohan Batra <rohanbatra24@gmail.com>
2020-03-10 20:18:24 +0000

    final

--- timer2.js
+++ timer2.js
@@ -1,13 +1,17 @@
 const stdin = process.stdin;
-// don't worry about these next two lines of setup work.
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
-
-process.stdin.on('data', (key) => {
-  setTimeout(() => {
-    // if (key === 'b') {
-    //   console.log('now');
-    // }
-    console.log(Number(key));
-  }, key * 1000);
+console.log('Menu:\nPress 0 - 9 to set a timer\nb to beep now\nctrl + c to quit');
+stdin.on('data', (key) => {
+  if (key === 'b') {
+    console.log('Beep!');
+  } else if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  } else {
+    console.log(`Setting timer for ${key} seconds`);
+    setTimeout(() => {
+      console.log(`Beep! (${Number(key)} seconds elapsed)`);
+    }, Number(key) * 1000);
+  }
 });
